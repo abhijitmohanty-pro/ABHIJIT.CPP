61b62165badf5e60246016859a84bd8ac984c1c9
abhijitmohanty-pro <abhijitmohanty@gmail.com>
2025-12-26 01:33:13 +0530

    cpp

--- project2.cpp
+++ project2.cpp
@@ -5,39 +5,40 @@
 #include <algorithm>
 using namespace std;
 int triplet(vector<int>vec,int target){
-     vector< vector<int>>ans;
-    sort(vec.begin(),vec.end());
-    int n=vec.size();
-    for(int i=0;i<n;i++){
-        if(i>0 && vec[i-1]==vec[i]) continue;
-        for(int j=i+1;j<n;j++){
-            if(vec[j-1]==vec[j]) continue;
-            int a=j+1,b=n-1; 
-            while(a<b){
-                if(a>j+1 && vec[a-1]==vec[a]) a++;
-                if(vec[i]+vec[j]+vec[a]+vec[b]==0){
-                    ans.push_back({vec[i],vec[j],vec[a],vec[b]});
-                    a++ ,b--;
-                }
-                else if(vec[i]+vec[j]+vec[a]+vec[b]>0)  b--;
-                else a++;    
+    vector<vector<int>>stored;
+    vector<int>prestored;
+     int sum=0,p=0;
+     int n=vec.size();
+     for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+        sum+=vec[j];
+        if(sum==target){
+             p++;
+            for(int k=i;k<=j;k++){
+                prestored.push_back(vec[k]);
             }
+            stored.push_back(prestored);
         }
+        prestored.clear();
+      }
+        sum=0;
     }
-    
-    for( auto i:ans){
-        for(auto j:i){
-            cout<<j<<" ";
-        } 
-        cout<<endl;   
-    }
+     cout<<p<<endl;
+     for(auto i:stored){
+        for(auto j:prestored){
+            cout<<j<<"  ";
+        }
+        cout<<endl;
+     }  
 }
 
+
 int main(){
-    vector<int >vec={1,0,-1,0,-2,2};
+    vector<int >vec={0,1,2,-1,3,-2};
     int target;
-    cout<<"give a target";
+    cout<<"give a target : ";
     cin>>target;
     triplet(vec,target);
+    
     return 0;
 }
