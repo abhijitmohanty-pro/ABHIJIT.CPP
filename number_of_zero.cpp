52c9aebfffbd3cf6b6207f3b4182063a9be98618
abhijitmohanty-pro <abhijitmohanty@gmail.com>
2025-12-28 23:02:28 +0530

    cpp

---
+++ number_of_zero.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <string>
+using namespace std;
+void print(string y){
+    vector<string>vec;
+    int n=y.length();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            vec[i];
+            for(int k=j;j<n;j++){
+                cout<<
+            }
+    }
+}
+}
+int main(){
+    string x="abc";
+    print(x);
+}
+  
